efe5af4c6af5f7db9c7802e10d04b57d83a008a2
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-19 14:21:51 +0300

    completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence:true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+
+    validates :category, inclusion: {in: ['Non-Fiction', 'Fiction']}
+
+    #custom validator
+    validate :title_sufficiently_clickbait
+
+    VALID_WORDS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def title_sufficiently_clickbait
+        errors.add(:title, 'must be clickbait') unless VALID_WORDS.any? { |pattern| title =~ pattern }
+    end
+
 end
